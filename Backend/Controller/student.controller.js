a64db49df3436f36073b80b7df0a7718fdabbfb0
Adarsha Gautam <gautamadarsha08@gmail.com>
2026-01-13 21:04:42 +0545

    Has upadted the verification for the student post request

--- Backend/Controller/student.controller.js
+++ Backend/Controller/student.controller.js
@@ -7,6 +7,21 @@ exports.createStudent = async (req, res) => {
   try {
     const { name, email, address, class: studentClass } = req.body;
     const adminId = req.adminId; // fetched from auth middleware
+    if(name == null || email == null || address == null || studentClass == null) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    if (!adminId) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    //if the name ,email,address or class are already present in the database then return error
+    const existingStudent = await pool.query(
+      `SELECT * FROM students WHERE email = $1 AND name = $2`,
+      [email, name]
+    );
+    if (existingStudent.rows.length > 0) {
+      return res.status(400).json({ message: "Student with this email already exists" });
+    }
+
 
     const newStudent = await pool.query(
       `INSERT INTO students (name,  email, address, class, admin_id)
